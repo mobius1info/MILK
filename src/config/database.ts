3eed8104a08ab43a3009fb317a5b6f9f33dcffa9
mobius1info <komisarenko84@bk.ru>
2026-01-21 21:00:53 +0300

    Updated database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,4 @@
 export const DATABASE_CONFIG = {
-  SUPABASE_URL: import.meta.env.VITE_SUPABASE_URL || 'https://qstiybtwufspvrvnrkbp.supabase.co',
-  SUPABASE_ANON_KEY: import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzdGl5YnR3dWZzcHZydm5ya2JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyNDIwNjAsImV4cCI6MjA4MzgxODA2MH0.XU_y1gKPza4r_84aWtUVaKc5lP3fobXKuoNWIod1nuo',
+  SUPABASE_URL: import.meta.env.VITE_SUPABASE_URL || 'https://cbboaievahhobnzgqypn.supabase.co',
+  SUPABASE_ANON_KEY: import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNiYm9haWV2YWhob2JuemdxeXBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyNjY2NzAsImV4cCI6MjA4MTg0MjY3MH0.siMaiYCdqu4Isynn63AvNSAPvGsTxbGkp2bxx1XAa8o',
 } as const;
